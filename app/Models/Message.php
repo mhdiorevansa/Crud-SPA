7f55e9a16e421f6e3bba6c2d57b5d5c97bac2e01
Muhammad Dio Revansa <mhdiorevansa098@gmail.com>
2024-10-01 08:28:28 +0700

    install broadcast

---
+++ app/Models/Message.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Message extends Model
+{
+    use HasFactory;
+
+    public $table = 'messages';
+    protected $fillable = [
+        'user_id',
+        'text'
+    ];
+    
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function getTimeAttribute(): string
+    {
+        return date(
+            "d M Y, H:i:s",
+            strtotime($this->attributes['created_at'])
+        );
+    }
+}
